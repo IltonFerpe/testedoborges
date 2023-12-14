da225374c3043f4d42951ef0e9331a3e413fc631
ilton <129966216+IltonFerpe@users.noreply.github.com>
2023-12-13 21:10:20 -0300

    meu primeiro comite

---
+++ teste/teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by Ilton Ferpec on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
